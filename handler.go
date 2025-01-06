60ae48c5a34304d66dbd31cdc4ee82063351d3d2
teegoood <teegooodgood@gmail.com>
2025-01-06 15:24:23 +0700

    refactor: handler.go

---
+++ handler.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func wsHandler(hub *hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("error upgrate connection: %s", err)
+		return
+	}
+
+	client := newClient(conn, hub)
+	client.read()
+}
